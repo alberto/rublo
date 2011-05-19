4d0eb792cb522950ac28cfa8471f1c19350d7f23
alberto <alberto.rodriguez@biko2.com>
2011-05-19 21:13:54 +0200

    DRYed test runner

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -1,11 +1,14 @@
 require_relative 'post_generator_test'
 require_relative 'post_test'
 
-tests = PostTest.new
-tests.can_set_post_title
-tests.can_set_post_body
+test_classes = [PostTest, PostGeneratorTest]
 
-tests = PostGeneratorTest.new
-tests.generate_replaces_title_in_template
-tests.generate_replaces_body_in_template
-tests.generate_ignores_unknown_attributes
+test_classes.each do |klass|
+  k = klass.new
+  methods = k.methods - Object.methods - Test.instance_methods
+  methods.each do |method|
+    puts method.to_s
+    k.send(method)
+  end
+end
+
