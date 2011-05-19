5747d296c91fe1b6a9dc4e701acd64a2f8e9f6c7
alberto <alberto.rodriguez@biko2.com>
2011-05-19 20:38:51 +0200

    Replaces all attributes in post

--- test/post_generator_test.rb
+++ test/post_generator_test.rb
@@ -5,7 +5,10 @@ class PostGenerator
     @template = template
   end
   def generate post
-    @template.gsub!("##title##", post.attributes["title"])
+    post.attributes.each_pair do |key, value|
+      @template.gsub!("###{key}##", value)
+    end
+    @template
   end
 end
 
@@ -17,8 +20,14 @@ class PostGeneratorTest
     generated_post == "<h2>Foo</h2>"
   end
 
+  def generate_replaces_body_in_template
+    post = Post.new("title" => "Foo", "body" => "<p>Bar</p>")
+    generator = PostGenerator.new ('<h2>##title##</h2><article>##body##</article>')
+    generated_post = generator.generate post
+    generated_post == "<h2>Foo</h2><article><p>Bar</p></article>"
+  end
 end
 
 tests = PostGeneratorTest.new
 p tests.generate_replaces_title_in_template
-
+p tests.generate_replaces_body_in_template
