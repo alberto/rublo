62ced9df8db60fcedbd3ecc240df5b5fc8e3e08c
alberto <alberto.email@gmail.com>
2011-05-30 22:48:10 +0200

    Created class for TestRunner

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -4,47 +4,51 @@ end
 
 require_relative 'test'
 
-
-def humanize text
-  text.gsub!('_', ' ')
-  text
-end
-
-def colorize(text, color_code)
-  puts "#{color_code}  #{humanize text}\e[0m"
-end
-
-def red text
-  colorize(text, "\e[31m")
-end
-
-def green text
-  colorize(text, "\e[32m")
-end
-
-number_of_tests = 0
-failed_tests = 0
-test_classes = Test.descendants
-test_classes.each do |klass|
-  p klass
-  methods = klass.new.methods - Object.methods - Test.instance_methods
-  number_of_tests += methods.count
-  methods.each do |method|
-    begin
-      k = klass.new
-      k.send(method)
-      green method.to_s
-    rescue TestException => e      
-      failed_tests +=1
-      red method.to_s
-      red e.message
-    rescue Exception => e
-      failed_tests +=1
-      puts e.message
-      puts e.backtrace.inspect
+class TestRunner
+  def run
+    number_of_tests = 0
+    failed_tests = 0
+    test_classes = Test.descendants
+    test_classes.each do |klass|
+      p klass
+      methods = klass.new.methods - Object.methods - Test.instance_methods
+      number_of_tests += methods.count
+      methods.each do |method|
+        begin
+          k = klass.new
+          k.send(method)
+          green humanize(method.to_s)
+        rescue TestException => e      
+          failed_tests +=1
+          red humanize(method.to_s)
+          red e.message
+        rescue Exception => e
+          failed_tests +=1
+          puts e.message
+          puts e.backtrace.inspect
+        end
+      end
+      puts
     end
+    puts "#{number_of_tests} tests run, #{failed_tests} failed"    
   end
-  puts
-end
-puts "#{number_of_tests} tests run, #{failed_tests} failed"
+  
+  def humanize text
+    text.gsub!('_', ' ')
+    text
+  end
 
+  def colorize(text, color_code)
+    puts "#{color_code}  #{text}\e[0m"
+  end
+
+  def red text
+    colorize(text, "\e[31m")
+  end
+
+  def green text
+    colorize(text, "\e[32m")
+  end
+end
+
+TestRunner.new.run
