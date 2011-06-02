bddf3bb2d71bf572e750e5a579207d2ba383d1f0
alberto <alberto.email@gmail.com>
2011-06-02 20:16:12 +0200

    Adding comments. :)

--- lib/index_generator.rb
+++ lib/index_generator.rb
@@ -24,7 +24,7 @@ class IndexGenerator
   def parse_partial posts
     parsed = ""
     posts.each do |post|
-      parsed << @posts_parser.parse(post.fields)
+      parsed << @posts_parser.parse(@settings.merge(post.fields))
     end
     parsed
   end
